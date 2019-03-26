1b3b3ea9e3cb995bd52f9f856cbd653b45733fab
Rafael Melikyan <melikyanrafael13@gmail.com>
2019-03-26 23:02:52 +0600

    Первый образец интерфейса

---
+++ Diplom/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Diplom {
+    partial class Form1 {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent() {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.OpenFile = new System.Windows.Forms.OpenFileDialog();
+            this.ToChooseFile = new System.Windows.Forms.Button();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.CodeAnalysis = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 391);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(810, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            this.statusStrip1.Visible = false;
+            // 
+            // OpenFile
+            // 
+            this.OpenFile.RestoreDirectory = true;
+            // 
+            // ToChooseFile
+            // 
+            this.ToChooseFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ToChooseFile.Location = new System.Drawing.Point(2, 46);
+            this.ToChooseFile.Name = "ToChooseFile";
+            this.ToChooseFile.Size = new System.Drawing.Size(125, 35);
+            this.ToChooseFile.TabIndex = 1;
+            this.ToChooseFile.Text = "Выберите файл";
+            this.ToChooseFile.UseVisualStyleBackColor = true;
+            this.ToChooseFile.Click += new System.EventHandler(this.ToChooseFile_Click);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Location = new System.Drawing.Point(482, 46);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // CodeAnalysis
+            // 
+            this.CodeAnalysis.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CodeAnalysis.Location = new System.Drawing.Point(2, 98);
+            this.CodeAnalysis.Name = "CodeAnalysis";
+            this.CodeAnalysis.Size = new System.Drawing.Size(125, 43);
+            this.CodeAnalysis.TabIndex = 3;
+            this.CodeAnalysis.Text = "Начать анализ участка кода";
+            this.CodeAnalysis.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(810, 413);
+            this.Controls.Add(this.CodeAnalysis);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.ToChooseFile);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.OpenFileDialog OpenFile;
+        private System.Windows.Forms.Button ToChooseFile;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button CodeAnalysis;
+    }
+}
+
---
+++ Diplom/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Diplom {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+            OpenFile.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+        }
+
+        private void ToChooseFile_Click(object sender, EventArgs e) {
+            if (OpenFile.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filePath = OpenFile.FileName;
+            statusStrip1.Visible = true;
+            toolStripStatusLabel1.Visible = true;
+            toolStripStatusLabel1.Text = filePath;
+        }
+    }
+}
