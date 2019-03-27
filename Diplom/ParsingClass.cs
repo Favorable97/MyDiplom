89f735960c7fc1f179b482500dff9b10d26792d5
Rafael Melikyan <melikyanrafael13@gmail.com>
2019-03-27 18:16:28 +0600

    В класс добавлен метод, который парсит текст и пару вспомогательных методов

--- Diplom/ParsingClass.cs
+++ Diplom/ParsingClass.cs
@@ -28,7 +28,64 @@ namespace Diplom {
             }
         }
 
-        
+        /*
+         * Разбиваем строку на массив строк, где разделитель - символ перехода на новую строчку.
+         * Каждую строчку проверяем, используя регулярное выражение.
+         * Если строка подходит, то записываем её во временную строку а также запоминаем строчку с началом цикла. 
+         * Так до тех пор, пока не будет найдена последняя закрывающая скобка.
+         * mas[0] == mas[1]
+         * Дойдя до конца блока, записываем его в специальный список, но прежде форматируем
+         */
+        public void ParsingText() {
+            ReadFile(ref textInFile);
+            string[] splitText = textInFile.Split('\n');
+            Regex regex = new Regex(@"\w*for\w*");
+            Regex pattern = new Regex(@"\s+");
+
+            for (int i = 0; i < splitText.Length; i++) {
+                if (regex.IsMatch(splitText[i])) {
+                    indexList.Add(i + 1);
+                    string strWithFor = "";
+                    do {
+                        string str = splitText[i];
+                        SearchOpenBracket(str);
+                        SearchCloseBracket(str);
+                        strWithFor += str;
+                        i++;
+
+                    } while (mas[0] != mas[1]);
+                    i--;
+                    mas[0] = 0;
+                    mas[1] = 0;
+                    //lstWithBlocks.Add(pattern.Replace(strWithFor.Trim('\t'), "\n"));
+                    lstWithBlocks.Add(strWithFor);
+
+                }
+
+            }
+            PrintList(lstWithBlocks);
+        }
+
+        private static void SearchOpenBracket(string str) {
+            int pos = 0;
+            while (str.IndexOf('{', pos) != -1) {
+                mas[0]++;
+                pos = str.IndexOf('{', pos) + 1;
+            }
+        }
+
+        private static void SearchCloseBracket(string str) {
+            int pos = 0;
+            while (str.IndexOf('}', pos) != -1) {
+                mas[1]++;
+                pos = str.IndexOf('}', pos) + 1;
+            }
+        }
+
+        public void PrintList(List<string> lst) {
+            foreach (var strok in lst)
+                Console.WriteLine(strok);
+        }
 
     }
 }
